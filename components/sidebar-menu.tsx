33efdc80d7e1d7bd802a728efac4b64151132754
YudiYamada <yudiyamada10@gmail.com>
2026-04-08 16:37:48 -0300

    style: modify styles

--- components/sidebar-menu.tsx
+++ components/sidebar-menu.tsx
@@ -7,7 +7,7 @@ interface SidebarMenuProps {
 const SidebarMenu = ({ icon, text, isActive }: SidebarMenuProps) => {
   return (
     <div
-      className={`flex h-11 w-63.5 items-center gap-2 rounded-lg pl-6 hover:cursor-pointer ${isActive ? "bg-primary/55" : "bg-transparent"}`}
+      className={`flex h-11 w-full items-center gap-2 rounded-lg pl-6 hover:cursor-pointer ${isActive ? "bg-primary/20" : "bg-transparent"}`}
     >
       <span
         className={`${isActive ? "text-primary" : "text-muted-foreground"}`}
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,7 +2,7 @@ import Nav from "./nav";
 
 const Sidebar = () => {
   return (
-    <div className="bg-background flex w-68 flex-col">
+    <div className="bg-card-background flex w-68 flex-col">
       <h1 className="text-primary pt-6 pl-8 text-2xl font-black">STOCKYA</h1>
 
       <Nav />

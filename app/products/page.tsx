75c25c660839e937c69fd7d62baeb87a63a35107
YudiYamada <yudiyamada10@gmail.com>
2026-04-08 15:47:07 -0300

    feat: add initial pages

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+const Products = () => {
+  return (
+    <main className="mt-8">
+      <h1>Produtos</h1>
+    </main>
+  );
+};
+
+export default Products;
---
+++ app/sales/page.tsx
@@ -0,0 +1,9 @@
+const Sales = () => {
+  return (
+    <main className="mt-8">
+      <h1>Vendas</h1>
+    </main>
+  );
+};
+
+export default Sales;

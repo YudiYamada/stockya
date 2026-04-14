02aee48478667636feb2bcc95040164393298148
YudiYamada <yudiyamada10@gmail.com>
2026-04-14 17:58:24 -0300

    feat: add page not found

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <main className="mt-8">
+      <h1>Página não encontrada!</h1>
+      <p>A página que você está procurando não existe.</p>
+    </main>
+  );
+};
+
+export default NotFound;

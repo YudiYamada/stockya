d8cca1af0098f0182982f5ba5cf484fed19ac10e
YudiYamada <yudiyamada10@gmail.com>
2026-05-06 19:51:17 -0300

    feat: add feaching products

--- data-access/product/get-products.ts
+++ data-access/product/get-products.ts
@@ -1,8 +1,18 @@
 import "server-only";
 
+import { unstable_cache } from "next/cache";
+
 import { Product } from "@/app/generated/prisma/client";
 import { db } from "@/lib/prisma";
 
 export const getProducts = async (): Promise<Product[]> => {
   return db.product.findMany({});
 };
+
+export const cachedGetProducts = unstable_cache(
+  getProducts,
+  ["get-products"],
+  {
+    revalidate: 5, // Revalidate every 5 seconds
+  }
+)

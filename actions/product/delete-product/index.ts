29239cc5065d94ede1bf16651800ecba5b3a1bd3
YudiYamada <yudiyamada10@gmail.com>
2026-05-25 23:24:57 -0300

    feat: ensure delete action use next-safe-action

--- actions/product/delete-product/index.ts
+++ actions/product/delete-product/index.ts
@@ -1,15 +1,28 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { returnValidationErrors } from "next-safe-action";
 
 import { db } from "@/lib/prisma";
+import { actionClient } from "@/lib/safe-action";
 
 import { DeleteProductSchema, deleteProductSchema } from "./schema";
 
-export const deleteProduct = async ({ id }: DeleteProductSchema) => {
-  deleteProductSchema.parse({ id });
-  await db.product.delete({
-    where: { id },
+export const deleteProduct = actionClient
+  .schema(deleteProductSchema)
+  .action(async ({ parsedInput }) => {
+    const data: DeleteProductSchema = parsedInput;
+
+    const product = await db.product.findUnique({ where: { id: data.id } });
+    if (!product) {
+      returnValidationErrors(deleteProductSchema, {
+        id: { _errors: ["Produto não encontrado."] },
+      });
+    }
+
+    await db.product.delete({
+      where: { id: data.id },
+    });
+
+    revalidatePath("/products");
   });
-  revalidatePath("/products");
-};
--- app/products/_components/delete-dialog-content.tsx
+++ app/products/_components/delete-dialog-content.tsx
@@ -1,4 +1,6 @@
 import { deleteProduct } from "@/actions/product/delete-product";
+import { flattenValidationErrors } from "next-safe-action";
+import { useAction } from "next-safe-action/hooks";
 import {
   AlertDialogAction,
   AlertDialogCancel,
@@ -17,16 +19,28 @@ interface DeleteProductDialogContentProps {
 const DeleteDialogContent = ({
   productId,
 }: DeleteProductDialogContentProps) => {
+  const { execute: executeDeleteProduct } = useAction(deleteProduct, {
+    onError: ({ error: { validationErrors } }) => {
+      const flat = flattenValidationErrors(validationErrors);
+      if (flat.formErrors?.length) {
+        toast.error(flat.formErrors[0]);
+      } else {
+        toast.error(
+          "Erro ao deletar",
+          "Ocorreu um erro ao tentar deletar o produto.",
+        );
+      }
+    },
+    onSuccess: () => {
+      toast.success("Produto deletado", "O produto foi removido com sucesso.");
+    },
+  });
+
   const handleContinueClick = async () => {
     try {
-      await deleteProduct({ id: productId });
-      toast.success("Produto deletado", "O produto foi removido com sucesso.");
+      await executeDeleteProduct({ id: productId });
     } catch (error) {
       console.error("Error ao deletar produto:", error);
-      toast.error(
-        "Erro ao deletar",
-        "Ocorreu um erro ao tentar deletar o produto.",
-      );
     }
   };
 

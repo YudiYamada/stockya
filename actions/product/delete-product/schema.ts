499045dd3c05f7f2997a08a077e60d803fdbf984
YudiYamada <yudiyamada10@gmail.com>
2026-05-28 18:32:26 -0300

    refactor: change db databases and remove unused files

--- actions/product/delete-product/schema.ts
+++
@@ -1,7 +0,0 @@
-import z from "zod";
-
-export const deleteProductSchema = z.object({
-  id: z.string().uuid(),
-});
-
-export type DeleteProductSchema = z.infer<typeof deleteProductSchema>;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="mt-8 w-full px-8">
-      <h1>Dashboard</h1>
-    </main>
-  );
-}
